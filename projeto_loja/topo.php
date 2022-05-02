f81c4efc4e98dbd7cd72b73695a157a2d8db5af2
EdGFischer <83289692+EdGFischer@users.noreply.github.com>
2022-05-02 13:41:01 -0300

    Add files via upload

---
+++ projeto_loja/topo.php
@@ -0,0 +1,15 @@
+<body>
+
+    <nav class="shadow p-2">
+        <section class="container d-flex flex-row justify-content-between align-items-center">
+            <div>LOGO DA EMPRESA</div>
+            <div class="d-flex flex-row align-items-center justify-items-center">
+                <div class="mx-3">
+                    <i class="fa-solid fa-cart-arrow-down fa-2x"></i>
+                </div>
+                <div class="mx-3">
+                    <i class="fa-solid fa-circle-user fa-3x"></i>
+                </div>
+            </div>
+        </section>
+    </nav>
