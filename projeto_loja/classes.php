fb655e6dd3ea3f8e55b6ede6a3c23aba871f86d1
Junior041 <ismaeljunior1508@gmail.com>
2022-06-15 13:49:17 -0300

    criada classes

---
+++ projeto_loja/classes.php
@@ -0,0 +1,23 @@
+<?php 
+
+
+
+Class Produto
+{
+    private $conexao;
+    
+    public function __construct(mysqli $conexao)
+    {
+        $this->conexao = $conexao;
+    }
+    public function exibirNoEstoque(int $id): array
+    {   
+        $selecionaProduto = $this->conexao->prepare("SELECT * FROM 'tbl_produto' WHERE id = ?");
+
+        $selecionaProduto->bind_Param('i',$id);
+        $selecionaProduto->execute();
+        $produtos = $selecionaProduto->get_result()->fetch_assoc();
+
+        return $produtos;
+    }
+}
