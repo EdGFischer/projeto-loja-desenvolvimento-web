5fce24f670576f3545bd6d368aa2c19c5c8f4dff
Junior041 <ismaeljunior1508@gmail.com>
2022-06-21 08:37:05 -0300

    fazendo filro

--- projeto_loja/admin/alterar-produto-post.php
+++ projeto_loja/admin/alterar-produto-post.php
@@ -6,4 +6,4 @@ $nome = $_POST['nome'];
 
 $query = "UPDATE teste SET nome = '$nome' WHERE id = '$id'"; // sql para execução
 $result = $conexao->query($query); // executa query
-header("Location: ./index.php");
+header("Location: modificar-produto.php");
