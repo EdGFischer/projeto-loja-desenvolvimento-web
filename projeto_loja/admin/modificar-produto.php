0d6294dd740fde89d393a09616fa248d4238161d
litoralcar <criacao1@litoralcar.com.br>
2022-06-21 09:57:50 -0300

    Alterações Front

--- projeto_loja/admin/modificar-produto.php
+++ projeto_loja/admin/modificar-produto.php
@@ -10,24 +10,30 @@ $result = $result->fetch_all(MYSQLI_ASSOC); // recupera valores de saída;
 ?>
 
 <section class="d-flex flex-column align-items-center justify-content-center w-100" style="height: 100vh;">
-    <div class="card">
+    <div class="card w-75">
         <div class="card-body rounded bg-dark">
-            <h5 class="card-title text-center text-light txt-uppercase mb-3">Deletar produto</h5>
-            
-            <?php foreach($result as $d){
-            ?>
-                <div style="width:100%;">
-                    <span> Id do produto = <?= $d['id'] ?></span>
-                    ||
-                    <span> nome do produto = <?= $d['nome'] ?></span>
-                    ||
-                    <span> preço do produto = <?= $d['preco'] ?></span>
-                    ||
-                    <span> <a href="deletar-produto-get.php?id=<?= $d['id'] ?>">X</a> </span>
-                    <span> <a href="alterar-produto.php?id=<?= $d['id'] ?>">Modificar</a> </span>
+            <h5 class="card-title text-center text-light txt-uppercase mb-3">Editar produto</h5>
 
-                </div>
-            <?php } ?>
+            <ul class="list-group p-0">
+                <li class="w-100 d-flex justify-content-center list-group-item">
+                    <div class="col-lg-6 d-flex flex-column justify-content-center fw-bold">Nome</div>
+                    <div class="col-lg-2 d-flex flex-column justify-content-center fw-bold">Preço</div>
+                    <div class="col-lg-2 d-flex flex-column justify-content-center fw-bold">Categoria</div>
+                    <div class="col-lg-1 d-flex flex-row justify-content-center fw-bold">Editar</div>
+                    <div class="col-lg-1 d-flex flex-row justify-content-center fw-bold">Remover</div>
+                </li>
+                <?php foreach ($result as $d) {
+                ?>
+
+                    <li class="w-100 d-flex justify-content-center list-group-item">
+                        <div class="col-lg-6 d-flex flex-column justify-content-center"><?= $d['nome'] ?></div>
+                        <div class="col-lg-2 d-flex flex-column justify-content-center">R$ <?= $d['preco'] ?></div>
+                        <div class="col-lg-2 d-flex flex-column justify-content-center"><?= $d['categoria'] ?></div>
+                        <div class="col-lg-1 d-flex flex-row justify-content-center"> <a href="alterar-produto.php?id=<?= $d['id'] ?>" class="btn btn-warning"><i class="fa-solid fa-file-pen"></i></a> </div>
+                        <div class="col-lg-1 d-flex flex-row justify-content-center"> <a href="deletar-produto-get.php?id=<?= $d['id'] ?>" class="btn btn-danger"><i class="fa-solid fa-trash-can" style="color:#000"></i></a> </div>
+                    </li>
+                <?php } ?>
+            </ul>
         </div>
     </div>
 
